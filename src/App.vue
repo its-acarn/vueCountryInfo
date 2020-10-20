f2de43e652aa835dd3a494af05f4dab69e219986
svvishjs <andreww92@live.com>
2020-10-20 16:19:14 +0100

    all countries added to list and can be selected

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template lang="html">
 	<main>
 		<h1>Country Info</h1>
-		<select name="country-dropdown" v-on:click="fetchCountry">
-			<option v-for="country in countries">{{ country.name }}</option>
+		<label for="country-dropdown"></label>
+		<select
+			name="country-dropdown"
+			id="country-dropdown"
+			v-model="selectedCountryIndex"
+			v-on:change="findCountryInfo()"
+		>
+			<option disabled value="">Select a country</option>
+			<option
+				v-for="(country, index) in countries"
+				:key="index"
+				:value="index"
+				>{{ country.name }}</option
+			>
 		</select>
+		<p>The selected country is {{ selectedCountryInfo.name }}</p>
+		<!-- <country-info
+			v-for="(country, index) in countries"
+			:key="index"
+			:country="country"
+		></country-info> -->
 	</main>
 </template>
 
 <script>
+import CountryInfo from './components/CountryInfo.vue';
+
 export default {
 	name: 'App',
 	data() {
 		return {
-			countries: []
+			countries: null,
+			selectedCountryIndex: null,
+			selectedCountryInfo: [{ name: '' }]
 		};
+	},
+	components: {
+		'country-info': CountryInfo
 	},
 	mounted() {
 		this.fetchCountry();
@@ -23,6 +48,11 @@ export default {
 			fetch('https://restcountries.eu/rest/v2/all')
 				.then((response) => response.json())
 				.then((data) => (this.countries = data));
+		},
+		findCountryInfo: function() {
+			this.selectedCountryInfo = this.countries[
+				this.selectedCountryIndex
+			];
 		}
 	}
 };
--- src/components/CountryInfo.vue
+++ src/components/CountryInfo.vue
@@ -1,7 +1,14 @@
-<template> </template>
+<template>
+	<hgroup>
+		<h2>{{ country.name }}</h2>
+	</hgroup>
+</template>
 
 <script>
-export default {};
+export default {
+	name: 'country-info',
+	props: ['country']
+};
 </script>
 
 <style></style>
